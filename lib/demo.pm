89790217aa0b29e16748ef4d72744e4a4b9c9ce7
Calle Dybedahl <calle@dybedahl.se>
2018-10-19 11:28:30 +0200

    Initial import.

---
+++ lib/demo.pm
@@ -0,0 +1,34 @@
+package demo;
+use Dancer ':syntax';
+use DBI;
+use Data::Dumper;
+
+our $VERSION = '0.1';
+
+our $dbi = DBI->connect(
+                        config->{dbconnect},
+                        config->{dbuser},
+                        config->{dbpass},
+                        {RaiseError => 1, PrintError => 0}
+                       ) or die $DBI::errstr;
+
+# Try to create our table, just ignore any problems (such as it already existing).
+eval {
+    $dbi->do(q[CREATE TABLE demodata (address varchar, uri varchar, tstamp integer)]);
+};
+
+my $store_sth = $dbi->prepare(q[INSERT INTO demodata VALUES (?,?,?)]);
+my $fetch_sth = $dbi->prepare(q[SELECT address, uri, tstamp FROM demodata ORDER BY tstamp DESC LIMIT 100]);
+
+get qr{/(.*)} => sub {
+    $store_sth->execute(request->address, request->uri, time());
+    my $data = $dbi->selectall_arrayref($fetch_sth);
+    for my $row (@$data) {
+        # Convert time_t value to readable string
+        $row->[2] = scalar(localtime($row->[2]))
+    }
+
+    template 'index', {rows => $data};
+};
+
+true;
